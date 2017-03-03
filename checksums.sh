fa69e8c9f0026d85efee492c6be83cdcc8878d51
ainsleya <ainsley.gibson@gmail.com>
2017-03-03 05:11:57 -0500

    Add files via upload

---
+++ checksums.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#Ainsley A. Gibson
+#For providing checksums for each file transferred. 
+#Fri Mar  3 04:30:44 EST 2017
+
+export COMPRESSED=OFS[1,2].tar.gz
+
+for files in $COMPRESSED
+
+#do sha256sum ${COMPRESSED} >> checksums.txt
+do sha256sum ${COMPRESSED} > checksums.txt
+
+done
+
+echo "Checksums complete"  
---
+++ transfer.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#Ainsley A. Gibson
+#For file transfer of x-data to NCEI
+#Fri Mar  3 04:05:45 EST 2017
+#
+#export OFS=OFS1
+export OFS=OFS[1,2]
+for files in $OFS
+
+#do tar -cvvf ${files}.tar ${files}; #Just for globbing
+do tar -zcvvf ${files}.tar.gz ${files}; #globbing with some compression
+
+done
+
+echo "Check it Ainsley!"
